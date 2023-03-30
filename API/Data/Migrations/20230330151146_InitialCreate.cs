baafad2ed9360da996effe2e821a54c401e7c714
Vijeth Belle <vijeth11@yahoo.co.in>
2023-03-30 21:01:55 +0530

    initialCommit

---
+++ API/Data/Migrations/20230330151146_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+            migrationBuilder.InsertData("Users", new[] { "Id", "UserName" }, new object[] { 1, "bob" });
+            migrationBuilder.InsertData("Users", new[] { "Id", "UserName" }, new object[] { 2, "tom" });
+            migrationBuilder.InsertData("Users", new[] { "Id", "UserName" }, new object[] { 3, "jane" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
